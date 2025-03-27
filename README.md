7ae3dbac94f3ee7750197304525e3f5e4c81d8cc
CosmicTiger <luisangelmarcia@gmail.com>
2025-03-27 07:31:53 -0600

    ✨ feature: add initial draft of the project for BloC install

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# forms_app
+# 📋 forms_app
+
+![Dart](https://img.shields.io/badge/dart-%230175C2.svg?style=for-the-badge&logo=dart&logoColor=white)
+![Flutter](https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&logo=Flutter&logoColor=white)
+![iOS](https://img.shields.io/badge/iOS-000000?style=for-the-badge&logo=ios&logoColor=white)
+![Android](https://img.shields.io/badge/Android-3DDC84?style=for-the-badge&logo=android&logoColor=white)
+![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
+![Android Studio](https://img.shields.io/badge/android%20studio-346ac1?style=for-the-badge&logo=android%20studio&logoColor=white)
 
 A new Flutter project.
---
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class BlocCounterScreen extends StatelessWidget {
+  const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bloc Counter'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.refresh_rounded),
+          ),
+        ],
+      ),
+      body: Center(
+        child: const Text('Counter value: xxxx'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1',
+            onPressed: () {},
+            child: const Text('+3'),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            heroTag: '2',
+            onPressed: () {},
+            child: const Text('+2'),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            heroTag: '3',
+            onPressed: () {},
+            child: const Text('+1'),
+          ),
+        ],
+      ),
+    );
+  }
+}
